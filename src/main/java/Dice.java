a1892d39a648b2727703ee20866259efe4985bbc
Chanelle <chanelle@Zipcoders-MacBook-Pro-103.local>
2023-07-09 03:14:58 -0400

    Submitting DiceyLab, all test running

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,18 @@
+
+import java.util.Random;
+
 public class Dice {
 
+    public Integer toss(){
+        Random rand = new Random();
+
+        int num = rand.nextInt(6);
+
+        num += 1;
+
+        return  num;
+    }
 
 }
+// Add 1 to the result to get a number from the required range
+
---
+++ src/test/java/SimulationTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Collection;
+public class SimulationTest {
+
+    @Test
+    public void test() {
+
+    }
+}
